41992dc70fe8db3f2a66dead6fdf49a27ba3c843
Asma <as1588ma@gmail.com>
2022-04-07 21:48:28 -0400

    complete homework

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+const employee = { name: "Sam" };
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return {
+    ...employee,
+    [key]: value,
+  };
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+  employee[key] = value;
+  return employee;
+}
+function deleteFromEmployeeByKey(employee, keyParam) {
+  return Object.entries(employee).reduce((newObject, [key, value]) => {
+    if (keyParam === key) {
+      return newObject;
+    }
+    return {
+      ...newObject,
+      [key]: value,
+    };
+  }, {});
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  employee[key] = undefined;
+  return employee;
+}
